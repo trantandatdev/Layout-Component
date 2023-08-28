19ea14a5a9360f3d4a0cb309f548d8677a6fe8d2
trantandatdev <trantandatdev@gmail.com>
2023-08-28 16:31:22 +0700

    up bai

--- src/PageContent/PageContent.js
+++ src/PageContent/PageContent.js
@@ -9,7 +9,7 @@ export default class PageContent extends Component {
           <div className="col-lg-6 col-xxl-4 mb-5">
             <div className="card bg-light border-0 h-100">
               <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-collection" /></div>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="fa fa-layer-group"></i></div>
                 <h2 className="fs-4 fw-bold">Fresh new layout</h2>
                 <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
               </div>
@@ -18,7 +18,7 @@ export default class PageContent extends Component {
           <div className="col-lg-6 col-xxl-4 mb-5">
             <div className="card bg-light border-0 h-100">
               <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-cloud-download" /></div>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="fa fa-download"></i></div>
                 <h2 className="fs-4 fw-bold">Free to download</h2>
                 <p className="mb-0">As always, Start Bootstrap has a powerful collectin of free templates.</p>
               </div>
@@ -27,7 +27,7 @@ export default class PageContent extends Component {
           <div className="col-lg-6 col-xxl-4 mb-5">
             <div className="card bg-light border-0 h-100">
               <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-card-heading" /></div>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="fa fa-poll-h"></i></div>
                 <h2 className="fs-4 fw-bold">Jumbotron hero header</h2>
                 <p className="mb-0">The heroic part of this template is the jumbotron hero header!</p>
               </div>
@@ -36,7 +36,7 @@ export default class PageContent extends Component {
           <div className="col-lg-6 col-xxl-4 mb-5">
             <div className="card bg-light border-0 h-100">
               <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-bootstrap" /></div>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="fab fa-bootstrap"></i></div>
                 <h2 className="fs-4 fw-bold">Feature boxes</h2>
                 <p className="mb-0">We've created some custom feature boxes using Bootstrap icons!</p>
               </div>
@@ -45,7 +45,7 @@ export default class PageContent extends Component {
           <div className="col-lg-6 col-xxl-4 mb-5">
             <div className="card bg-light border-0 h-100">
               <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-code" /></div>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="fa fa-code"></i></div>
                 <h2 className="fs-4 fw-bold">Simple clean code</h2>
                 <p className="mb-0">We keep our dependencies up to date and squash bugs as they come!</p>
               </div>
@@ -54,7 +54,7 @@ export default class PageContent extends Component {
           <div className="col-lg-6 col-xxl-4 mb-5">
             <div className="card bg-light border-0 h-100">
               <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i className="bi bi-patch-check" /></div>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="fa fa-check-circle"></i></div>
                 <h2 className="fs-4 fw-bold">A name you trust</h2>
                 <p className="mb-0">Start Bootstrap has been the leader in free Bootstrap templates since 2013!</p>
               </div>
